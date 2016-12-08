e93dced7f1841fd17627465d124898b7a6d17c34
Subhranil Mukherjee <iamsubhranil@github.com>
2016-12-08 22:44:06 +0530

    Use a long * to keep track of next node's address Previously, to keep track of the address of the next node, after an iteration, current node was being backed up. Upon next iteration, after calling malloc() for the allocation of new node, the nextaddress member of previously backed up node was being updated by the newly created node's address. i.e. prevnode->nextaddress = (long)node; This process was both tedious and memory consuming. This is resolved in this commit. Now, a long * nextaddress is created and allocated at runtime. Upon a iteration, the pointer is reassigned to the address of the nextaddress member of present node. i.e. nextaddress = &node->nextaddress; On the next iteration, after malloc(), the value of nextaddress is updated with the memory address being allocated as *nextaddress = (long)node; This process is obviously more efficient and more modular, as no extra space is being used. Meanwhile, start is also changed from long to long *, to keep up with nextaddress. On traversal, the pointer nextaddress is getting reassigned as a long * at the address stored in node->nextaddress. i.e. nextaddress = (long *)node->nextaddress;

--- misc_list.c
+++ misc_list.c
@@ -14,68 +14,69 @@ struct node_type_real {
 
 void main(){
 	int conti = 1,ntype,lastused = 0,*flag;
-	long start = 0,prevaddress = 0, nextaddress = 0;
-	struct node_type_int *intnode, *previntnode=NULL;
-	struct node_type_real *realnode, *prevrealnode=NULL;
+	long *start=NULL,prevaddress = 0, *nextaddress;
+	struct node_type_int *intnode;
+	struct node_type_real *realnode;
+	printf("\nInitializing pointers..\n");
+	nextaddress = (long *)malloc(sizeof(long));
 	while(conti){
-		printf("Which type of node do you want to enter ?\nEnter 1 for integer\n2 for real : ");
+		printf("Which type of node do you want to insert ?\nEnter 1 for integer\n2 for real : ");
 		scanf("%d",&ntype);
 		if(ntype==1){
 			intnode = (struct node_type_int *)malloc(sizeof(struct node_type_int));
-			if(lastused==1 && previntnode!=NULL){
-				previntnode->nextaddress = (long)intnode;
+			if(lastused>0){
+				*nextaddress = (long)intnode;
 			}
-			else if(prevrealnode!=NULL){
-				prevrealnode->nextaddress = (long)intnode;
-			}
-			if(start==0){
-				start = (long)intnode;
-				printf("start is assigned to address %ld\n",start);
+			if(start==NULL){
+				start = (long *)intnode;
 			}
 			intnode->flag = 1;
 			printf("Enter int data to store : ");
 			scanf("%d",&intnode->data);
 			intnode->nextaddress = 0;
 			lastused = 1;
-			previntnode = intnode;
+			nextaddress = &intnode->nextaddress;
 		}
 		else if(ntype==2){
 			realnode = (struct node_type_real *)malloc(sizeof(struct node_type_real));
-			if(lastused==1 && previntnode!=NULL){
-				previntnode->nextaddress = (long)realnode;
-			}
-			else if(prevrealnode!=NULL){
-				prevrealnode->nextaddress = (long)realnode;
-			}			
-			if(start==0){
-				start = (long)realnode;
-				printf("start is assigned to address %ld\n",start);
+			if(lastused>0){
+				*nextaddress = (long)realnode;
+			}		
+			if(start==NULL){
+				start = (long *)realnode;
 			}
 			realnode->flag = 2;
 			printf("Enter real data to store : ");
 			scanf("%f",&realnode->data);
 			realnode->nextaddress = 0;
 			lastused = 2;
-			prevrealnode = realnode;
+			nextaddress = &realnode->nextaddress;
 		}
 		else{
 			break;
 		}	
 	}
 	nextaddress = start;
+	printf("\nStarting traversal from address %ld\n",(long)nextaddress);
 	while(nextaddress!=0){
 		flag = (int *)nextaddress;
 		if((*flag)==1){
 			printf("\nNode is int\n");
 			intnode = (struct node_type_int *)nextaddress;
-			printf("Data : %d\n",intnode->data);
-			nextaddress = intnode->nextaddress;
+			printf("Data : %d",intnode->data);
+			nextaddress = (long *)intnode->nextaddress;
+			printf("\nNext address : %ld\n",intnode->nextaddress);
 		}
-		else{
+		else if((*flag)==2){
 			printf("\nNode is real\n");
 			realnode = (struct node_type_real *)nextaddress;
-			printf("Data : %f\n",realnode->data);
-			nextaddress = realnode->nextaddress;
+			printf("Data : %g",realnode->data);
+			nextaddress = (long *)realnode->nextaddress;
+			printf("\nNext address : %ld\n",realnode->nextaddress);
+		}
+		else{
+			printf("Unknown flag %d!\n",*flag);
+			break;
 		}
 	}
 	
